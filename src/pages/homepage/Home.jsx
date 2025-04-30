58d2e87b012063a10bf4df2890dc5146c4b97d4a
Anuragkrenai <anurag@krenai.com>
2025-04-30 18:21:21 +0530

    fixed font size

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -5,22 +5,22 @@ const Home = () => {
       <div className="mainContainer h-[90%] w-[85%]  rounded-2xl overflow-hidden">
         <div className="header bg-[#111a16] h-[14%] flex items-center justify-between px-10 ">
           <div>
-            <p className="flex items-end gap-3 text-[14px] text-[#9f9fa8]">
+            <p className="flex items-end gap-3 text-[1.2vw] text-[#9f9fa8]">
               Hi, I am
-              <h3 className="text-[28px] text-[#fff] font-[600] leading-none">
+              <h3 className="text-[2.4vw] text-[#fff] font-[600] leading-none">
                 Sasta Hacker
               </h3>{" "}
-              <span className="text-[18px] transition-all duration-300 handShake">
+              <span className="text-[1.8vw] transition-all duration-300 handShake">
                 👋
               </span>
             </p>
-            <p className="text-[14px] mt-2 text-[#9f9fa8]">
+            <p className="text-[1.2vw] mt-2 text-[#9f9fa8]">
               Full Stack Developer crafting exceptional digital experiences{" "}
-              <span className="text-[#64f196]">Krenai Private Limited</span>
+              <span className="text-[#64f196]">Softtech Private Limited</span>
             </p>
           </div>
 
-          <div className="flex items-center gap-3 text-[11px] text-[#9f9fa8] bg-[#1c2121] border-[1px] border-[#2e3033] cursor-pointer transition-all duration-300 hover:border-[#bebebe] rounded-3xl px-4 py-1">
+          <div className="flex items-center gap-3 text-[1vw] text-[#9f9fa8] bg-[#1c2121] border-[1px] border-[#2e3033] cursor-pointer transition-all duration-300 hover:border-[#bebebe] rounded-3xl px-4 py-1">
             <div className="flex items-center gap-2 relative border">
               <div className="w-1.5 h-1.5 bg-[#88db9b] rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] smallDot"></div>
               <div className="w-1.5 h-1.5 bg-[#2bb84d] rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] "></div>
