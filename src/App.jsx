1b64c133dbccaf9540df3e10e5d1ef72645d0703
Anuragkrenai <anurag@krenai.com>
2025-05-06 00:18:06 +0530

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   )
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,56 @@
-import './Projects.css'
+import "./Projects.css";
+import { NavLink } from "react-router-dom";
+import { FaUserTie } from "react-icons/fa6";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { TiContacts } from "react-icons/ti";
+import { HiOutlineDocumentText } from "react-icons/hi2";
+
 
 const Projects = () => {
-  return (
-    <div className='h-[90%] w-[85%] bg-[#111a16] rounded-2xl pageAnimation'>Projects</div>
-  )
-}
 
-export default Projects
+  return (
+    <div className="h-[90%] w-[85%]  rounded-2xl overflow-hidden pageAnimation">
+      <div className="header bg-[#111a16] h-[14%] flex items-center justify-between px-7 ">
+      <div>
+          <div className="flex items-center gap-3 text-[1.2vw] text-[#9f9fa8]">
+            <h3 className="text-[2.4vw] text-[#fff] font-[600] leading-none text-shadow-2xl">
+              Projects
+            </h3>{" "}
+            <span className="text-[1.8vw] text-white transition-all duration-300 projectBounce">
+              <HiOutlineDocumentText />
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-[3vw]">
+          <dir className="flex items-center justify-end  gap-[1vw] navlinkContainer">
+            <NavLink
+              to="/"
+              className="text-[1.2vw] text-[#9f9fa8] h-[2.5vw] w-[2.5vw] cursor-pointer bg-[#18181b] flex items-center justify-center  border-[1px] border-[#2e3033] hover:border-[#bebebe] transition-all duration-300   hover:text-white rounded-full "
+            >
+              <FaUserTie />
+            </NavLink>
+
+            <NavLink
+              to="/projects"
+              className="text-[1.3vw]  text-[#9f9fa8] h-[2.5vw] w-[2.5vw] cursor-pointer bg-[#18181b] flex items-center justify-center  border-[1px] border-[#2e3033] hover:border-[#bebebe] transition-all duration-300   hover:text-white rounded-full "
+            >
+              <IoDocumentTextOutline />
+            </NavLink>
+
+            <NavLink
+              to="/contact"
+              className="text-[1.3vw]  text-[#9f9fa8] h-[2.5vw] w-[2.5vw] cursor-pointer bg-[#18181b] flex items-center justify-center  border-[1px] border-[#2e3033] hover:border-[#bebebe] transition-all duration-300  hover:text-white rounded-full "
+            >
+              <TiContacts />
+            </NavLink>
+          </dir>
+        </div>
+      </div>
+
+      <div className="main bg-[#111111] h-[86%] flex flex-col justify-between gap-5 px-7 py-5"></div>
+    </div>
+  );
+};
+
+export default Projects;
