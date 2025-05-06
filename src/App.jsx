fb8f9968cbe560718dee9bccbf504079b9d9bef9
Anuragkrenai <anurag@krenai.com>
2025-05-06 10:28:32 +0530

    added contact and projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Lazy, Suspense} from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/homepage/Home';
 import Projects from './pages/projects/Projects';
