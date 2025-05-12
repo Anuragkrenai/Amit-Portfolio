2b81d76df9bfb11b2c228f7f2dc530b16366c7e9
Anuragkrenai <anurag@krenai.com>
2025-05-12 16:42:09 +0530

    form handling completed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -14,7 +14,12 @@ import { FaGithub } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import { useState } from "react";
 import { FaSpinner } from "react-icons/fa";
+import { VscDebugRestart } from "react-icons/vsc";
+
+import { FaSkullCrossbones } from "react-icons/fa6";
+
 import { TiTick } from "react-icons/ti";
+import axios from 'axios'
 
 
 
@@ -30,9 +35,12 @@ const Contact = () => {
   const [emailErr, setEmailErr] = useState(false)
   const [messageErr, setMessageErr] = useState(false)
   const [loading,setLoading] = useState(false)
+  const [messageState, setMessageState] = useState('idle')
   const [buttonMgs, setButtonMsg] = useState(<span className="flex items-center gap-[0.5vw]">Send Message <IoSend  className="text-[1vw]"/></span>)
 
 
+  const URL = 'https://dummyjson.com/test'
+  // const URL = 'https://dummyjsoaasdfn.com/test'
 
   function handleError(){
     !message.name? setNameErr(true) : setNameErr(false)
@@ -41,26 +49,57 @@ const Contact = () => {
   }
 
 
+  async function postData (){
+    try{
+      const response = await axios.get(URL);
+      console.log(response)
+      if(response.status === 200){
+        setLoading(false)
+        setMessageState('success')  
+        setButtonMsg(<span className="flex items-center gap-[0.5vw] ">Message Sent <TiTick  className="text-[1.6vw] mb-[0.3vw]"/></span>)
+        setMessage({
+          name:'',
+          email:'',
+          message:''
+        })
+        setTimeout(()=>{
+          setButtonMsg(<span className="flex items-center gap-[0.5vw]">Redirecting...  <FaSpinner className="animate-spin text-[1.2vw]" /></span>)
+          setTimeout(()=>{
+             navigate('/')
+          },1000)
+        },2000)
+      }
+      else{
+        setLoading(false)
+        setMessageState('failed')
+        setButtonMsg(<span className="flex items-center gap-[0.5vw] ">Message Falied <FaSkullCrossbones  className="text-[1.1vw] mb-[0.3vw]"/></span>)
+        setTimeout(()=>{
+          setButtonMsg(<span className="flex items-center gap-[0.5vw]">Resend Message  <VscDebugRestart  className="animate-spin text-[1.2vw]" /></span>)
+        },2000)
+      }
+    }
+    catch{
+      setLoading(false)
+      setMessageState('failed')
+      setButtonMsg(<span className="flex items-center gap-[0.5vw] ">Resend Message <VscDebugRestart  className="text-[1.1vw] mb-[0.3vw]"/></span>)
+    }
+  }
+
+
+
+
   const handleSubmit =(e)=>{
     e.preventDefault();
+
 
     if(!message.name || !message.email || !message.message){
       handleError()
     }
     else{
       handleError()
+      setMessageState('idle')
       setLoading(true)
-      setTimeout(()=>{
-        setLoading(false)
-        setButtonMsg(<span className="flex items-center gap-[0.5vw] ">Message Sent <TiTick  className="text-[1.6vw] mb-[0.3vw]"/></span>)
-        setTimeout(()=>{
-          setButtonMsg(<span className="flex items-center gap-[0.5vw]">Redirecting...  <FaSpinner className="animate-spin text-[1.2vw]" /></span>)
-          setTimeout(()=>{
-            // navigate('/')
-          },1000)
-        },2000)
-        
-      },2000)
+      postData()
     }
   }
 
@@ -122,28 +161,28 @@ const Contact = () => {
            <p className="text-[1.6vw] text-[#d4d4d8] flex items-center gap-3">Let's Discuss Your Project <TiMessages className="mb-[0.4vw]"/></p>
            <form action="" className="flex flex-col gap-8 justify-between h-full " onSubmit={handleSubmit}>
               <div className="w-full shrink-0 relative ">
-                <input type="text" placeholder="Your Name *" onChange={(elem)=>{ setMessage({...message, name : elem.target.value}); }} value={message.name} className={`w-full  border  rounded-lg  ${nameErr? ' border-[#c33434] focus:border-[#c33434] hover:border-[#c33434]' : ' border-[#272928] hover:border-[#10b98140] focus:border-[#10b98140]'} transition-all duration-300 outline-none px-[1vw] py-[0.7vw] text-[1.2vw] bg-[#1e2020] text-[#d4d4d8] placeholder:text-[#9e9e9e]`}/>
-                {nameErr?(<p className="text-[#c33434] text-[1.1vw] absolute top-[102%] left-[0.3vw]">Name is required</p>) :''}  
+                <input type="text" placeholder="Your Name *" onChange={(elem)=>{ setMessage({...message, name : elem.target.value}); elem.target.value ? setNameErr(false) : setNameErr(true)}} value={message.name} className={`w-full  border  rounded-lg  ${nameErr? ' border-[#c33434] focus:border-[#c33434] hover:border-[#c33434]' : ' border-[#272928] hover:border-[#10b98140] focus:border-[#10b98140]'} transition-all duration-300 outline-none px-[1vw] py-[0.7vw] text-[1.2vw] bg-[#1e2020] text-[#d4d4d8] placeholder:text-[#9e9e9e]`}/>
+                {nameErr?(<p className="text-[#c33434] text-[1.1vw] absolute bottom-[-22px] left-[0.3vw]">Name is required</p>) :''}  
               </div>
               
               <div className="w-full shrink-0 relative ">
-                <input type="email" placeholder="Your Email *" onChange={(elem)=>{ setMessage({...message, email : elem.target.value})}} value={message.email}  className={`w-full  border  rounded-lg  ${emailErr? ' border-[#c33434] focus:border-[#c33434] hover:border-[#c33434]' : ' border-[#272928] hover:border-[#10b98140] focus:border-[#10b98140]'} transition-all duration-300 outline-none px-[1vw] py-[0.7vw] text-[1.2vw] bg-[#1e2020] text-[#d4d4d8] placeholder:text-[#9e9e9e]`} />
-                {emailErr?(<p className="text-[#c33434] text-[1.1vw] absolute top-[102%] left-[0.3vw]">Email is required</p>) :''}
+                <input type="email" placeholder="Your Email *" onChange={(elem)=>{ setMessage({...message, email : elem.target.value}); elem.target.value ? setEmailErr(false) : setEmailErr(true)}} value={message.email}  className={`w-full  border  rounded-lg  ${emailErr? ' border-[#c33434] focus:border-[#c33434] hover:border-[#c33434]' : ' border-[#272928] hover:border-[#10b98140] focus:border-[#10b98140]'} transition-all duration-300 outline-none px-[1vw] py-[0.7vw] text-[1.2vw] bg-[#1e2020] text-[#d4d4d8] placeholder:text-[#9e9e9e]`} />
+                {emailErr?(<p className="text-[#c33434] text-[1.1vw] absolute bottom-[-22px] left-[0.3vw]">Email is required</p>) :''}
               </div>
 
               <div className="w-full h-full relative">
-                <textarea name="your-message" placeholder="Your Message *" onChange={(elem)=>{setMessage({...message, message : elem.target.value})}} value={message.message}  className={`w-full h-full resize-none  border  rounded-lg  ${messageErr? ' border-[#c33434] focus:border-[#c33434] hover:border-[#c33434]' : ' border-[#272928] hover:border-[#10b98140] focus:border-[#10b98140]'} transition-all duration-300 outline-none px-[1vw] py-[0.7vw] text-[1.2vw] bg-[#1e2020] text-[#d4d4d8] placeholder:text-[#9e9e9e]`}></textarea>
-                {messageErr?(<p className="text-[#c33434] text-[1.1vw] absolute top-[102%] left-[0.3vw]">Message is required</p>) :''}
+                <textarea name="your-message" placeholder="Your Message *" onChange={(elem)=>{setMessage({...message, message : elem.target.value}); elem.target.value ? setMessageErr(false) : setMessageErr(true)}} value={message.message}  className={`w-full h-full resize-none  border  rounded-lg  ${messageErr? ' border-[#c33434] focus:border-[#c33434] hover:border-[#c33434]' : ' border-[#272928] hover:border-[#10b98140] focus:border-[#10b98140]'} transition-all duration-300 outline-none px-[1vw] py-[0.7vw] text-[1.2vw] bg-[#1e2020] text-[#d4d4d8] placeholder:text-[#9e9e9e]`}></textarea>
+                {messageErr?(<p className="text-[#c33434] text-[1.1vw] absolute bottom-[-22px] left-[0.3vw]">Message is required</p>) :''}
               </div>
 
-              <button className={`${loading ? 'pointer-events-none' : 'pointer-events-auto' } select-none relative w-max px-[1vw] py-[1vw] overflow-hidden border rounded-lg border-[#272928] text-[#9e9e9e]  hover:border-[#10b74d] hover:text-[#10b74d] transition-all duration-300 bg-[#1e2020] cursor-pointer`}>
+              <button  className={`${loading ? 'pointer-events-none cursor-default' : 'pointer-events-auto' } ${messageState === 'failed' ? 'border-[#c33434] text-[#c33434] hover:border-[#c33434] focus:border-[#c33434] hover:text-[#c33434]' : ''} ${messageState === 'success' ? 'pointer-events-none cursor-default border-[#272928] hover:border-[#10b74d] focus:border-[#10b74d] hover:text-[#10b74d] text-[#10b74d]' : ''} ${messageState === 'idle' ? 'border-[#272928] hover:border-[#10b74d] focus:border-[#10b74d] hover:text-[#10b74d] text-[#d4d4d8]' : ''}   select-none relative w-max px-[1vw] py-[1vw] overflow-hidden border rounded-lg    outline-none  transition-all duration-300 bg-[#1e2020] cursor-pointer`}>
                  <div className="p-0 h-full min-w-[10vw] text-[1.2vw] z-[3]  flex items-center justify-center gap-[0.5vw] cursor-pointer">
                   {
                    !loading ? buttonMgs : <span className="flex items-center gap-[0.5vw]">Sending  <FaSpinner className="animate-spin text-[1.2vw]" /> </span>   
                   }
                  
                  </div>
-                 <div className="absolute h-full  top-0 left-[-45%] shadowAnimation "></div>
+                 <div className={`absolute h-full  top-0 left-[-45%] shadowAnimation  ${messageState === 'failed' ? 'shadow-[0px_0px_20px_30px_rgba(195,52,52,0.42)]' : 'shadow-[0px_0px_20px_30px_rgba(16,183,77,0.42)]'}`}></div>
               </button>
            </form>
         </div>
